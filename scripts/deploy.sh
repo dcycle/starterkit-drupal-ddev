cefd4f8908ebd361c388001169a3539f9721f44c
Albert Albala <albert@mediatribe.net>
2026-04-21 10:23:57 -0400

    Please squash this commit in the pull request

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ddev start
 ddev composer install
-ddev drush site:install -y
+ddev drush site:install minimal -y
+ddev drush sqlc < ./starter-data/initial.sql
 ddev describe
--- scripts/export-database.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-ddev drush sql:dump --ordered-dump \
-  --skip-tables-list=cache,cache_* > \
-  ./do-not-commit/dumpfile.sql
-ddev drush sql:dump --ordered-dump \
-  --structure-tables-list=cache,cache_* >> \
-  ./do-not-commit/dumpfile.sql
