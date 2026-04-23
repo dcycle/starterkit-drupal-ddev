676249505d21770285aa14b337684b37e5bef654
Albert Albala <albert@mediatribe.net>
2026-04-23 10:23:46 -0400

    Test for admin users (#3)

--- test/playwright/tests/article-list.spec.ts
+++ test/playwright/tests/article-list.spec.ts
@@ -14,3 +14,19 @@ test('Anonymous users see 5 latest published articles.', async ({ page }) => {
   // Only five articles per page, so article one should not be visible.
   await expect(page.locator('text=ArticleOne')).toHaveCount(0);
 });
+
+test('Admins users see 5 latest articles, whether they are published or not.', async ({ page }) => {
+  await page.goto('https://starterkit-drupal-ddev.ddev.site/user/login');
+  await page.fill('input[name="name"]', 'admin');
+  console.log('Using the password from the environment variable ADMIN_PASSWORD: ' + process.env.ADMIN_PASSWORD);
+  await page.fill('input[name="pass"]', process.env.ADMIN_PASSWORD!);
+  await page.click('input.form-submit');
+  await page.click('text=Home');
+  // Article 7 is unpublished, so it should not be visible to anonymous
+  // users.
+  // In admin mode there might be several items with the text ArticleSeven,
+  // for example, which Playwright does not like. So we'll target instead
+  // the link to the article, which is unique.
+  await expect(page.locator('h2 a[href="/node/7"]')).toBeVisible();
+  await expect(page.locator('h2 a[href="/node/6"]')).toBeVisible();
+});
