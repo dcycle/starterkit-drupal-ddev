d9584a6170e7a3f36541e323543664999a00d674
Albert Albala <albert@mediatribe.net>
2026-05-04 13:31:19 -0400

    Run playwright directly with no Ddev extension (#5)

--- test/playwright/article-list.spec.ts
+++ test/playwright/article-list.spec.ts
@@ -1,11 +1,7 @@
 import { test, expect } from '@playwright/test';
 
-test.use({
-  ignoreHTTPSErrors: true,
-});
-
 test('Anonymous users see 5 latest published articles.', async ({ page }) => {
-  await page.goto('https://starterkit-drupal-ddev.ddev.site');
+  await page.goto('http://web');
   // Article 7 is unpublished, so it should not be visible to anonymous
   // users.
   await expect(page.locator('text=ArticleSeven')).toHaveCount(0);
@@ -16,7 +12,7 @@ test('Anonymous users see 5 latest published articles.', async ({ page }) => {
 });
 
 test('Admins users see 5 latest articles, whether they are published or not.', async ({ page }) => {
-  await page.goto('https://starterkit-drupal-ddev.ddev.site/user/login');
+  await page.goto('http://web/user/login');
   await page.fill('input[name="name"]', 'admin');
   console.log('Using the password from the environment variable ADMIN_PASSWORD: ' + process.env.ADMIN_PASSWORD);
   await page.fill('input[name="pass"]', process.env.ADMIN_PASSWORD!);
