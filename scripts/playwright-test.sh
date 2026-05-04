df46b49b00573db0f95dec39441cf2a66127a988
Albert Albala <albert@mediatribe.net>
2026-05-04 13:25:13 -0400

    Please squash this commit in the pull request

--- scripts/playwright-test.sh
+++ scripts/playwright-test.sh
@@ -2,5 +2,11 @@
 # Get a random password
 PASSWORD=$(openssl rand -base64 12)
 ddev drush upwd admin $PASSWORD
-# https://github.com/Lullabot/ddev-playwright/issues/77
-ddev exec -s web /bin/bash -c "export ADMIN_PASSWORD=$PASSWORD && cd test/playwright && yarn && yarn playwright test"
+docker run --rm \
+  -v "$(pwd)/test/playwright":/app/test \
+  -e ADMIN_PASSWORD=$PASSWORD \
+  --network ddev-starterkit-drupal-ddev_default \
+  -w /app \
+  --ipc=host \
+  mcr.microsoft.com/playwright:v1.59.1-noble \
+  bash -c "npm init -y && npm install @playwright/test && npx playwright test test"
